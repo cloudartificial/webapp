de83cf1565425966debab0a182290432062b61ff
cloudartificial <sabir@cloudartificial.com>
2025-11-29 11:40:45 +0500

    webapp base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'YOUR-REPO-NAME' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: 'webapp', 
+  base: '/webapp/', 
 })
