cc7fa726ffb4c47083e26d2b63028c0bd278f806
cloudartificial <sabir@cloudartificial.com>
2025-11-29 11:59:39 +0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'YOUR-REPO-NAME' with your actual GitHub repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/webapp/', 
-})
